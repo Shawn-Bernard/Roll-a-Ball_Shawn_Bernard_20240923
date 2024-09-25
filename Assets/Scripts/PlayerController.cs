5d65a3cd442f3fae961175f563ecaad6a29a9532
Shawn Bernard <ssbernard@outlook.com>
2024-09-25 10:42:16 -0300

    Done (need to add something)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,9 +12,14 @@ public class PlayerController : MonoBehaviour
     private float movementY;
 
     public float speed = 0;
+    public float CoolDown = 2f;
+    public float Timer;
+    //public bool CanJump = true;
+    
 
     private int count;
     public TextMeshProUGUI countText;
+    public GameObject winTextCount;
     
      
     
@@ -24,6 +29,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText();
+        winTextCount.SetActive(false);
     }
     void OnMove(InputValue movementValue)
     {
@@ -34,12 +40,41 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
 
     }
+    void Update()
+    {
+        if (Timer > 0)
+        {
+            Timer -= Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && CoolDown <= 0)
+        {
+          rb.AddForce(0, 100, 0, ForceMode.Force);
+            Timer = CoolDown;
 
-    private void FixedUpdate()
+        }
+
+
+            //if (Input.GetKeyDown(KeyCode.Space) && CanJump <= 0 )
+            //{
+            //    rb.AddForce(0, 100, 0, ForceMode.Force);
+            //    CanJump -= -25;
+            //}
+            //else
+            //{
+            //    CoolDown += CanJump + Time.deltaTime;
+            //}
+
+        }
+
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+        
+        
+
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -51,9 +86,17 @@ public class PlayerController : MonoBehaviour
             count++;
             SetCountText();
         }
+        if (other.gameObject.CompareTag("SpeedBoost"))
+        {
+            speed = 20;
+        }
     }
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winTextCount.SetActive(true);
+        }
     }
 }
