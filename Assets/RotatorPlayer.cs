3f02248faa3ce817b645f1e00f1a4e23591dfe3a
Shawn Bernard <ssbernard@outlook.com>
2024-10-15 18:36:12 -0300

    Added enemy code that follows the player/ rotation code for flashlight(buggy)

---
+++ Assets/RotatorPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorPlayer : MonoBehaviour
+{
+    public Transform Player;
+    public float Speed = 5f;
+    private Vector3 offset;
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        
+        transform.RotateAround(Player.position, Vector3.up, mouseX * Speed );
+
+        
+
+    }
+}
