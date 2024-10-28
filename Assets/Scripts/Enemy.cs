ddaa4acb3fba9780f897beb4536d2f479016cb9b
Shawn Bernard <ssbernard@outlook.com>
2024-10-28 12:21:49 -0300

    (v2.01) Added Enemy mat & prefab Enemy Spawner

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,22 +9,24 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
 
       
         
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
         Follow();
+        //Away();
 
     }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Light")
         {
+            
             Destroy(gameObject);
         }
     }
@@ -32,5 +34,10 @@ public class Enemy : MonoBehaviour
     {
         GameObject P = GameObject.Find("Player");
         transform.position = Vector3.MoveTowards(this.transform.position, P.transform.position, 2 * Time.deltaTime);
+    }
+    void Away()
+    {
+        GameObject Safe = GameObject.Find("SafeLight");
+        transform.position = Vector3.MoveTowards(this.transform.position, Safe.transform.position, 1 - Time.deltaTime);
     }
 }
